aaab4fbbb714100cde7fb8f2735d750734535261
JorelAli <jorel.k.ali@gmail.com>
2019-03-02 11:25:56 +0000

    Enables rainbows & shrinks NERD width

--- configuration.nix
+++ configuration.nix
@@ -179,8 +179,36 @@
                 set number
                 set mouse=a
                 let g:airline_powerline_fonts = 1
+                let g:NERDTreeWinSize=20
                 autocmd vimenter * NERDTree
                 set backspace=indent,eol,start
+
+                let g:rbpt_colorpairs = [
+                    \ ['brown',       'RoyalBlue3'],
+                    \ ['Darkblue',    'SeaGreen3'],
+                    \ ['darkgray',    'DarkOrchid3'],
+                    \ ['darkgreen',   'firebrick3'],
+                    \ ['darkcyan',    'RoyalBlue3'],
+                    \ ['darkred',     'SeaGreen3'],
+                    \ ['darkmagenta', 'DarkOrchid3'],
+                    \ ['brown',       'firebrick3'],
+                    \ ['gray',        'RoyalBlue3'],
+                    \ ['black',       'SeaGreen3'],
+                    \ ['darkmagenta', 'DarkOrchid3'],
+                    \ ['Darkblue',    'firebrick3'],
+                    \ ['darkgreen',   'RoyalBlue3'],
+                    \ ['darkcyan',    'SeaGreen3'],
+                    \ ['darkred',     'DarkOrchid3'],
+                    \ ['red',         'firebrick3'],
+                    \ ]
+
+                let g:rbpt_max = 16
+                let g:rbpt_loadcmd_toggle = 0
+                
+                au VimEnter * RainbowParenthesesToggle
+                au Syntax * RainbowParenthesesLoadRound
+                au Syntax * RainbowParenthesesLoadSquare
+                au Syntax * RainbowParenthesesLoadBraces
             '';
             vimrcConfig.vam.knownPlugins = pkgs.vimPlugins;
 
